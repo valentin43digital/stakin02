a63b3d9c05c5e643fb50cff6b8611b113f4b20db
Valentin Alekseev <asted_asd@mail.ru>
2022-05-22 12:46:25 +0700

    Staking update

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,55 +1,58 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
-interface IToken {
-    function balanceOf(address _owner) external view returns(uint256);
-    function allowance(address _owner, address _spender) external view returns (uint256);
-    function transfer(address _to, uint256 _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-    function approve(address _spender, uint256 _value) external returns (bool success);
+interface ERC20Token {
+    function balanceOf(address owner) external view returns(uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
 }
+
 
 contract Staking {
     address public owner;
-    address private tokenAddr;
+    address private lpTokenAddress;
+    address private rewardTokenAddress;
+    uint public lpCourse;
     
     struct Staked {
         uint256 amount;
         uint256 reward;
         uint256 timeStamp;
     }
-    mapping(address =>Staked) private balances;
+    mapping(address => Staked) private stakeholders;
 
-    constructor(address _tokenAddr) {
-        tokenAddr = _tokenAddr;
+    constructor(address _lpTokenAddress, address _rewardTokenAddress) {
+        owner = msg.sender;
+        lpTokenAddress = _lpTokenAddress;
+        rewardTokenAddress = _rewardTokenAddress;
+        lpCourse = 2000;
     }
 
     function stake(uint256 amount) public {
-        require(balances[msg.sender].amount == 0, "Already staked");
-        balances[msg.sender].amount = amount;
-        balances[msg.sender].timeStamp = block.timestamp;
-        IToken(tokenAddr).transferFrom(msg.sender, address(this), amount);
-
+        require(stakeholders[msg.sender].amount == 0, "Already staked");
+        require(ERC20Token(lpTokenAddress).allowance(msg.sender, address(this)) >= amount, "No enough allowance");
+        ERC20Token(lpTokenAddress).transferFrom(msg.sender, address(this), amount);
+        stakeholders[msg.sender].amount = amount;
+        stakeholders[msg.sender].timeStamp = block.timestamp;
     }
 
     function claim() public {
-        require(balances[msg.sender].reward != 0, "Zero reward");
-        IToken(tokenAddr).transfer(msg.sender, balances[msg.sender].reward);
-        balances[msg.sender].reward = 0;
-        
+        require(stakeholders[msg.sender].reward != 0, "Zero reward");
+        ERC20Token(rewardTokenAddress).transfer(msg.sender, stakeholders[msg.sender].reward);
+        stakeholders[msg.sender].reward = 0;
     }
 
-    function unstake() public {
-        require(balances[msg.sender].amount != 0, "Zero balance staked");
-        //IToken(tokenAddr).transfer(msg.sender, balances[msg.sender].amount);
-        IToken(tokenAddr).transfer(msg.sender, balances[msg.sender].amount);
-        balances[msg.sender].amount = 0;
+    function unstake(uint256 amount) public {
+        require(stakeholders[msg.sender].amount != 0, "Zero balance staked");
+        require(stakeholders[msg.sender].amount >= amount, "Not enough balance staked");
+        ERC20Token(lpTokenAddress).transfer(msg.sender, amount);
+        stakeholders[msg.sender].amount -= amount;
     }
 
     function checkReward() public {
-        require(block.timestamp - balances[msg.sender].timeStamp > 600, "Time not passed");
-        balances[msg.sender].reward = balances[msg.sender].amount * 20 / 100;
+        require(block.timestamp - stakeholders[msg.sender].timeStamp > 600, "Time not passed");
+        stakeholders[msg.sender].reward = stakeholders[msg.sender].amount * 20 / 100 * lpCourse;
     }
 }
