2387fb4f284dd540788dcbd2e7571aca8d15c543
Valentin Alekseev <asted_asd@mail.ru>
2022-05-22 12:49:03 +0700

    Token addresses added

--- config.ts
+++ config.ts
@@ -5,6 +5,9 @@ const configts = {
     symbol: "VLTN",
     decimals: "18",
     amount1: "100.0",
-    amount2: "2000000.0"
+    amount2: "2000000.0",
+    amount3: "50.0",
+    LPTOKENADDRESS: "0x46e4ec8aAfeDd4b3f011703B8Fee052b7FfA0745",
+    REWARDTOKENADDRESS: "0xe15F64C33488F432A980e4c60Cf618E697A540b7",
 }
 export default configts
