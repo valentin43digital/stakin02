b34718cffc796772019d8f9bd0111d91cd4b7574
Valentin Alekseev <asted_asd@mail.ru>
2022-05-22 13:43:44 +0700

    Arguments for deploy added

---
+++ arguments.ts
@@ -0,0 +1,6 @@
+module.exports = [
+    "0x46e4ec8aAfeDd4b3f011703B8Fee052b7FfA0745",
+    "0xe15F64C33488F432A980e4c60Cf618E697A540b7",
+  ];
+
+// npx hardhat verify --network rinkeby --constructor-args arguments.ts 0x830736E9E5A24cA5D653505bfD97A92df558b8DC
